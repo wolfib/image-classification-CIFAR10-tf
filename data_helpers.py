ee147906ec4ca0e8e1e3c8c0b375c7dae7786b41
Wolfgang Beyer <wolfi.b@gmx.at>
2016-12-04 17:47:09 -0800

    initial commit

---
+++ data_helpers.py
@@ -0,0 +1,88 @@
+'''Imports CIFAR-10 data.'''
+
+import numpy as np
+import cPickle as pickle
+
+def load_CIFAR10_batch(filename):
+  '''load data from single CIFAR-10 file'''
+
+  with open(filename, 'rb') as f:
+    dict = pickle.load(f)
+    x = dict['data']
+    y = dict['labels']
+    x = x.astype(float)
+    y = np.array(y)
+  return x, y
+
+def load_data():
+  '''load all CIFAR-10 data and merge training batches'''
+
+  xs = []
+  ys = []
+  for i in xrange(1, 6):
+    filename = 'cifar-10-batches-py/data_batch_' + str(i)
+    X, Y = load_CIFAR10_batch(filename)
+    xs.append(X)
+    ys.append(Y)
+
+  x_train = np.concatenate(xs)
+  y_train = np.concatenate(ys)
+  del xs, ys
+
+  x_test, y_test = load_CIFAR10_batch('cifar-10-batches-py/test_batch')
+
+  classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
+    'ship', 'truck']
+
+  # Normalize Data
+  mean_image = np.mean(x_train, axis=0)
+  x_train -= mean_image
+  x_test -= mean_image
+
+  data_dict = {
+    'images_train': x_train,
+    'labels_train': y_train,
+    'images_test': x_test,
+    'labels_test': y_test,
+    'classes': classes
+  }
+  return data_dict
+
+def reshape_data(data_dict):
+  im_tr = np.array(data_dict['images_train'])
+  im_tr = np.reshape(im_tr, (-1, 3, 32, 32))
+  im_tr = np.transpose(im_tr, (0,2,3,1))
+  data_dict['images_train'] = im_tr
+  im_te = np.array(data_dict['images_test'])
+  im_te = np.reshape(im_te, (-1, 3, 32, 32))
+  im_te = np.transpose(im_te, (0,2,3,1))
+  data_dict['images_test'] = im_te
+  return data_dict
+
+#def generate_random_batch(images, labels, batch_size):
+  # Generate batch
+  #indices = np.random.choice(images.shape[0], batch_size)
+  #images_batch = images[indices]
+  #labels_batch = labels[indices]
+  #return images_batch, labels_batch
+
+def gen_batch(data, batch_size, num_iter):
+  data = np.array(data)
+  index = len(data)
+  for i in xrange(num_iter):
+    index += batch_size
+    if (index + batch_size > len(data)):
+      index = 0
+      shuffled_indices = np.random.permutation(np.arange(len(data)))
+      data = data[shuffled_indices]
+    yield data[index:index + batch_size]
+
+def main():
+  data_sets = load_data()
+  print(data_sets['images_train'].shape)
+  print(data_sets['labels_train'].shape)
+  print(data_sets['images_test'].shape)
+  print(data_sets['labels_test'].shape)
+
+if __name__ == '__main__':
+  main()
---
+++ softmax.py
@@ -0,0 +1,89 @@
+'''Softmax-Classifier for CIFAR-10'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+import time
+import data_helpers
+
+beginTime = time.time()
+
+# Parameter definitions
+batch_size = 100
+learning_rate = 0.005
+max_steps = 1000
+
+# Uncommenting this line removes randomness
+# You'll get exactly the same result on each run
+# np.random.seed(1)
+
+# Prepare data
+data_sets = data_helpers.load_data()
+
+# -----------------------------------------------------------------------------
+# Prepare the Tensorflow graph
+# (We're only defining the graph here, no actual calculations taking place)
+# -----------------------------------------------------------------------------
+
+# Define input placeholders
+images_placeholder = tf.placeholder(tf.float32, shape=[None, 3072])
+labels_placeholder = tf.placeholder(tf.int64, shape=[None])
+
+# Define variables (these are the values we want to optimize)
+weights = tf.Variable(tf.zeros([3072, 10]))
+biases = tf.Variable(tf.zeros([10]))
+
+# Define the classifier's result
+logits = tf.matmul(images_placeholder, weights) + biases
+
+# Define the loss function
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits,
+  labels_placeholder))
+
+# Define the training operation
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+# Operation comparing prediction with true label
+correct_prediction = tf.equal(tf.argmax(logits, 1), labels_placeholder)
+
+# Operation calculating the accuracy of our predictions
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# -----------------------------------------------------------------------------
+# Run the Tensorflow graph
+# -----------------------------------------------------------------------------
+
+with tf.Session() as sess:
+  # Initialize variables
+  sess.run(tf.initialize_all_variables())
+
+  # Repeat max_steps times
+  for i in range(max_steps):
+
+    # Generate input data batch
+    indices = np.random.choice(data_sets['images_train'].shape[0], batch_size)
+    images_batch = data_sets['images_train'][indices]
+    labels_batch = data_sets['labels_train'][indices]
+
+    # Perform a single training step
+    sess.run(train_step, feed_dict={images_placeholder: images_batch,
+      labels_placeholder: labels_batch})
+
+    # Periodically print out the model's current accuracy
+    if (i + 1) % 100 == 0:
+      train_accuracy = sess.run(accuracy, feed_dict={
+        images_placeholder: images_batch, labels_placeholder: labels_batch})
+      print('Step {:5d}: training accuracy {:g}'.format(i + 1, train_accuracy))
+
+
+  # After finishing the training, evaluate on the test set
+  test_accuracy = sess.run(accuracy, feed_dict={
+    images_placeholder: data_sets['images_test'],
+    labels_placeholder: data_sets['labels_test']})
+  print('Test accuracy {:g}'.format(test_accuracy))
+
+endTime = time.time()
+print('Total time: {:5.2f}s'.format(endTime - beginTime))
